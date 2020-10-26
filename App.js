f1131518ad16a140c4d7f97183e59dc644a064d9
Leudy <leudi_rosario@hotmail.com>
2020-10-26 01:44:38 -0400

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, StatusBar } from "react-native";
+import styled from "styled-components/native";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <StatusBar barStyle="light-content" />
+      <RecipeBackground source={require("./assets/mojave.jpeg")}>
+        <SafeAreaView></SafeAreaView>
+      </RecipeBackground>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  text-align: center;
+  background-color: #fff;
+`;
+
+const RecipeBackground = styled.ImageBackground`
+  width: 100%;
+`;
